02970822ba69c9cabaaa3e610c8dc26aaa18d583
Daoud <99m.daoud@gmail.com>
2020-10-15 14:00:43 +0200

    aggiunti es 3 e 4

--- js-jsnack3/main.js
+++ js-jsnack3/main.js
@@ -1,2 +1,18 @@
-// JSnack 2: L’utente inserisce due parole in successione, con due prompt.
-// Il software stampa prima la parola più corta, poi la parola più lunga.
+// JSnack 3: Il software deve chiedere per 5 volte all’utente di inserire un
+// numero. Il programma stampa la somma di tutti i numeri inseriti.
+var somma = 0;
+
+for (var i = 0; i < 5; i++) {
+    //inserimento numero
+    var numeroInserito = parseFloat(prompt("inserire il numero " + (i + 1)));
+    //verifica che línput sia effettivamente un numero
+    while(isNaN(numeroInserito)) {
+        numeroInserito = parseFloat(prompt("Il dato inserito non é valido. Inserire nuovamente il numero " + (i + 1)));
+    }
+    console.log("Il " + (i + 1) + "° numero inserito é: " + numeroInserito);
+    // somma del valore precedente di somma con il numero inserito in questa iterazione del ciclo for
+    somma = somma + numeroInserito;
+}
+
+// stampa del valore finale della somma
+console.log('La somma dei 5 numeri inseriti é: ' + somma);
---
+++ js-jsnack4/main.js
@@ -0,0 +1,22 @@
+// JSnack 4
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
+// un numero, se è dispari inseriscilo nell’array.
+
+var oddNumberBox = [];
+
+for (var i = 0; i < 6; i++) {
+
+    var numeroInserito = parseInt(prompt("Inserisci un numero"));
+
+    while(isNaN(numeroInserito)) {
+        numeroInserito = parseFloat(prompt("Il dato inserito non é valido. Inserire nuovamente il numero " + (i + 1)));
+    }
+
+    console.log("Il " + (i + 1) + "° numero inserito é: " + numeroInserito);
+
+    if (numeroInserito % 2 != 0) {
+        oddNumberBox.push(numeroInserito);
+    }
+}
+
+console.log(oddNumberBox);
